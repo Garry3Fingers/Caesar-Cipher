af6957b4b02484430eb422a9066fda1a615404b0
Garry3Fingers <www.vlad78@yandex.ru>
2022-12-10 18:39:58 +0300

    Create transformed_arr with bug
    It can't handle symbols and

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -8,7 +8,21 @@ end
 
 def caesar_cipher(string, shift_factor)
   converted_str_to_arr = convert_string_to_arr(string)
-  
+  transformed_arr = converted_str_to_arr.map do |arr|
+    arr.map do |chr|
+      if chr == chr.upcase
+        current_index = RANGE_UPCASE_CHR.find_index(chr)
+        new_index = current_index + 5
+        new_index = new_index - 25 if new_index > 25
+        chr = RANGE_UPCASE_CHR[new_index]
+      else
+        current_index = RANGE_LOWERCASE_CHR.find_index(chr)
+        new_index = current_index + 5
+        new_index = new_index - 25 if new_index > 25
+        chr = RANGE_LOWERCASE_CHR[new_index]
+      end
+    end
+  end
 
 end
 
